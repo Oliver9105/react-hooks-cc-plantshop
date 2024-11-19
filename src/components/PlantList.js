2f56482e65c9ff6d042360126e8370e4189031e5
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-19 20:19:16 +0300

    updated component

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,29 +1,27 @@
 import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({ plants, setPlants, searchedPlant }) { 
-
+function PlantList({ plants, setPlants, searchedPlant }) {
   useEffect(() => {
     fetch("http://localhost:6001/plants")
-      .then(res => res.json())
-      .then(data => setPlants(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
 
-  const filteredPlants = searchedPlant === "" ? 
-    [...plants] :
-    plants.filter(plant => plant.name.toLowerCase().includes(searchedPlant.toLowerCase()))
+  const filteredPlants =
+    searchedPlant === ""
+      ? [...plants]
+      : plants.filter((plant) =>
+          plant.name.toLowerCase().includes(searchedPlant.toLowerCase())
+        );
 
   function renderPlants() {
-    return filteredPlants.map(plant => {
-      return <PlantCard key={plant.id} plant={plant}/>
-    })
+    return filteredPlants.map((plant) => {
+      return <PlantCard key={plant.id} plant={plant} />;
+    });
   }
 
-  return (
-    <ul className="cards">
-      {renderPlants()}
-    </ul>
-  );
+  return <ul className="cards">{renderPlants()}</ul>;
 }
 
 export default PlantList;
